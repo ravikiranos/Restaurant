0fb1848d3c794a64ef233ed062412ecb7db35907
Ravi Kiran <ravi.140690@gmail.com>
2020-06-26 17:56:21 +0530

    Commit

---
+++ Restaurant/DashBoard.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Restaurant
+{
+    /// <summary>
+    /// Interaction logic for DashBoard.xaml
+    /// </summary>
+    public partial class DashBoard : Window
+    {
+        public DashBoard()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Sample objmainwinobj = new Sample();
+           
+
+            grid2.Children.Clear();
+
+            grid2.Children.Add(objmainwinobj);
+
+
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_5(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_6(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
